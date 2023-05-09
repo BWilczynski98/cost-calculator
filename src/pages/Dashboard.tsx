ea7dbc615f5bf91e0886e05cfc556ae1899c7551
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-05-09 18:49:05 +0200

    Libraries: Material UI, i18next. Settings eslint and prettier, clear project structure

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Grid from '@mui/material/Unstable_Grid2'
+
+export const Dashboard = () => {
+  return (
+    <Grid container>
+      <Grid xs={12}></Grid>
+      <Grid xs={12}></Grid>
+    </Grid>
+  )
+}
