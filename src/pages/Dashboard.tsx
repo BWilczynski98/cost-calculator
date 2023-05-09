be74f86749101d40cf99f705ef14aeb734921f70
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-05-09 20:39:44 +0200

    View of available services

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Grid from '@mui/material/Unstable_Grid2'
+import { ServiceList } from '../components/ServicesList'
 
 export const Dashboard = () => {
   return (
-    <Grid container>
-      <Grid xs={12}></Grid>
+    <Grid container sx={{padding: 2}}>
+      <Grid xs={12}>
+        <ServiceList />
+      </Grid>
       <Grid xs={12}></Grid>
     </Grid>
   )
